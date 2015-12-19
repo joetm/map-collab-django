32f71a4d0b679c94d860bf2d070c3f3272de70e0
J. Oppenlaender <j.oppenlaender@web.de>
2015-12-19 20:58:35 +0000

    app

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+    map = new L.Map('map', {layers: [osm], center: new L.LatLng(51.505, -0.04), zoom: 13}),
+    drawnItems = L.featureGroup().addTo(map);
+
+map.addControl(new L.Control.Draw({
+    edit: { featureGroup: drawnItems }
+}));
+
+map.on('draw:created', function(event) {
+    var layer = event.layer;
+
+    drawnItems.addLayer(layer);
+});
