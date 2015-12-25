76cb85b1e19931fd3ba540dfc8d6285e53c74ed3
J. Oppenlaender <j.oppenlaender@web.de>
2015-12-25 08:49:26 +0000

    app

--- api.php
+++ api.php
@@ -3,22 +3,49 @@
 //var_dump($_POST) won't work
 //get the request payload from the backbone call
 $input = json_decode(file_get_contents('php://input'), true);
-
 //var_dump($input);
-
-// save the feature to the DB...
 
 require_once('db.php');
 
 
-$s = $db->prepare("INSERT INTO geometry ( id, type, geometry, username, creation ) values ( NULL, :type, :geometry, :username, :creation)");
-//$s->bindParam(':geometry', $input['geometry']);
-//$s->bindParam(':type', $input['type']);
-//$s->bindParam(':username', 'Hans');
-//$s->bindParam(':creation', time());
-$input['username'] = 'Hans';
-$input['creation'] = time();
-$s->execute($input);
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+
+
+
+
+}
+elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+	$s = $db->query('SELECT id, type, geometry, username, creation, modified from geometry');
+ 
+	# setting the fetch mode
+	$s->setFetchMode(PDO::FETCH_ASSOC);
+
+	$gs = [];
+	while($row = $s->fetch()) {
+		$gs[] = $row;
+	}
+	
+	echo json_encode($gs);
+
+
+}
+elseif (in_array($_SERVER['REQUEST_METHOD'], array('PUT','POST'))) {
+
+	// upsert or save the feature
+
+	$s = $db->prepare("INSERT INTO geometry ( id, type, geometry, username, creation ) values ( NULL, :type, :geometry, :username, :creation)");
+	//$s->bindParam(':geometry', $input['geometry']);
+	//$s->bindParam(':type', $input['type']);
+	//$s->bindParam(':username', 'Hans');
+	//$s->bindParam(':creation', time());
+	$input['username'] = 'Hans';
+	$input['creation'] = time();
+	$s->execute($input);
+
+}
+
 
 # close the connection
-$DBH = null;
+$db = null;
--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
+/**************/
+/* Map Collab */
+/**************/
+
+/*global L, Backbone, console*/
+
 var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+    osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib}),
     map = new L.Map('map', {layers: [osm], center: new L.LatLng(51.505, -0.04), zoom: 13}),
     drawnItems = L.featureGroup().addTo(map),
     //Backbone
@@ -13,50 +19,88 @@ var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     });
 
 
+function addToMap(g) {
+    'use strict';
+
+    console.log('add event:', g);
+
+    var encoded = g.get('geometry');
+    var id = g.get('id');
+
+    switch (g.get('type')) {
+    case 'marker':
+        var m = L.Marker.fromEncoded(encoded).addTo(map);
+        break;
+    case 'polyline':
+        var pl = L.Polyline.fromEncoded(encoded).addTo(map);
+        break;
+    case 'polygon':
+        var pg = L.Polygon.fromEncoded(encoded).addTo(map);
+        break;
+    case 'circle':
+        var ci = L.Polyline.fromEncoded(encoded).addTo(map);
+        break;
+    case 'rectangle':
+        var rc = L.Polygon.fromEncoded(encoded).addTo(map);
+        break;
+    }
+
+    var polyline = L.Polyline.fromEncoded(encoded).addTo(map);
+
+}
+
+var geometries = new Features();
+
+geometries.on('add', addToMap);
+
+//populate map
+geometries.fetch();
+
+
 
 map.addControl(new L.Control.Draw({
     edit: {featureGroup: drawnItems}
 }));
 
-map.on('draw:created', function(event) {
+map.on('draw:created', function (event) {
+    'use strict';
     var layer = event.layer,
         type = event.layerType;
 
     layer = event.layer;
 
-    console.log(type, layer);
+    console.log('created:', type, layer);
 
     switch (type) {
-       case 'marker':
-            var marker = new Feature({
+    case 'marker':
+        var marker = new Feature({
                 type: type,
                 //latlng: layer._latlng,
                 //options: layer.options
                 layer: layer
             });
-            marker.save();
-            break;
-       case 'polyline':
-           //fall through
-       case 'polygon':
-           //fall through
-       case 'circle':
-           //fall through
-       case 'rectangle':
+        marker.save();
+        break;
+    case 'polyline':
+       //fall through
+    case 'polygon':
+       //fall through
+    case 'circle':
+       //fall through
+    case 'rectangle':
 
-            //polyline encoding with Leaflet plugin
-            var encoded = layer.encodePath();
-            console.log(encoded)
-
-            var model = new Feature({
+        //polyline encoding with Leaflet plugin
+        var encoded = layer.encodePath(),
+            model = new Feature({
                 type: type,
                 //latlngs: layer._latlngs,
                 //options: layer.options
                 //layer: layer
                 geometry: encoded
             });
-            model.save();
-            break;
+        console.log(encoded);
+        model.save();
+        break;
     }
 
     drawnItems.addLayer(layer);
