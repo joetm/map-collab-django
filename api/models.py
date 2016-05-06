9212ac3ea488be6433c4cd425f8ca7414e545a41
Jonas Oppenlaender <jonas.oppenlaender@liip.ch>
2016-05-06 03:03:29 -0700

    models

--- api/models.py
+++ api/models.py
@@ -2,4 +2,32 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Shape(models.Model):
+    name = models.CharField(max_length=15)
+    creation_date = models.DateTimeField('date created')
+    leafletJSON = models.TextField() # stores the Leaflet feature as a JSON object
+
+    center_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    center_lng = models.DecimalField(max_digits=9, decimal_places=6)
+
+    boundary_left   = models.DecimalField(max_digits=9, decimal_places=6)
+    boundary_right  = models.DecimalField(max_digits=9, decimal_places=6)
+    boundary_top    = models.DecimalField(max_digits=9, decimal_places=6)
+    boundary_bottom = models.DecimalField(max_digits=9, decimal_places=6)
+
+class Rectangle(Shape):
+    pass
+
+class Polygon(Shape):
+    pass
+
+class Line(Shape):
+    pass
+
+class Circle(Shape):
+    radius = models.FloatField()
+
+class Marker(Shape):
+    pass
+
--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'clientside.apps.ClientsideConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
